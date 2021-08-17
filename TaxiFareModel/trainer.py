6e243499741ad67784f480f7ddd3f0940077780a
Michael Weitnauer <michael.weitnauer@gmail.com>
2021-08-17 15:46:35 +0200

    remove evaluate method and added cross_val_score method instead; fixed mlflow logging

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -1,9 +1,10 @@
+from sklearn import metrics
 from ml_flow_test import EXPERIMENT_NAME
 import mlflow
 from mlflow.tracking import MlflowClient
 from memoized_property import memoized_property
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import LinearRegression
@@ -15,23 +16,24 @@ from TaxiFareModel.data import get_data, clean_data
 
 
 class Trainer():
-    EXPERIMENT_NAME = "[DE] [Munich] [kickermeister]"
-
     def __init__(self, X_train, y_train, exp_model='LinearRegression v1',
                  regressor=LinearRegression(), **kwargs):
         """
             X_train: pandas DataFrame
             y_train: pandas Series
         """
+        self.regressor = regressor
+        self.model_name = exp_model
         self.pipeline = self.set_pipeline(regressor)
         self.X_train = X_train
         self.y_train = y_train
 
         # Log to score and params to MLFlow server
         self._mlflow_uri = "https://mlflow.lewagon.co/"
-        self.experiment_name = EXPERIMENT_NAME + exp_model
-        self.mlflow_create_run()
+        self.mlflow_local = False
+        self.experiment_name = "[DE] [Munich] [kickermeister] TaxiFareModel"
         self.mlflow_log_param("model", exp_model)
+        self.mlflow_log_param("user", 'kickermeister')
         for key, value in kwargs.items():
             self.mlflow_log_param(key, value)
 
@@ -59,12 +61,21 @@ class Trainer():
         """set and train the pipeline"""
         self.pipeline.fit(self.X_train, self.y_train)
 
-    def evaluate(self, X_test, y_test):
+    def cross_val_score(self):
+        """Return cross-validated score"""
+        scores = cross_val_score(self.pipeline,
+                                 self.X_train,
+                                 self.y_train,
+                                 scoring='neg_root_mean_squared_error',
+                                 n_jobs=-1)
+        score = -1 * scores.mean()
+        self.mlflow_log_metric('RMSE', score)
+        return score
+
+    def predict(self, X_test):
         """evaluates the pipeline on df_test and return the RMSE"""
         y_pred = self.pipeline.predict(X_test)
-        rmse = compute_rmse(y_pred, y_test)
-        self.mlflow_log_metric('RMSE', rmse)
-        return rmse
+        return y_pred
 
     def save_model(self, save_name='model'):
         """ Save the trained model into a .joblib file """
@@ -72,7 +83,8 @@ class Trainer():
 
     @memoized_property
     def mlflow_client(self):
-        mlflow.set_tracking_uri(self._mlflow_uri)
+        if self.mlflow_local == False:
+            mlflow.set_tracking_uri(self._mlflow_uri)
         return MlflowClient()
 
     @memoized_property
@@ -87,9 +99,6 @@ class Trainer():
     def mlflow_run(self):
         return self.mlflow_client.create_run(self.mlflow_experiment_id)
 
-    def mlflow_create_run(self):
-        self.mlfow_run = self.mlflow_client.create_run(self.mlflow_experiment_id)
-
     def mlflow_log_param(self, key, value):
         self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
 
@@ -100,20 +109,23 @@ class Trainer():
 if __name__ == "__main__":
     df = get_data()
     df = clean_data(df)
-    y = df["fare_amount"]
-    X = df.drop("fare_amount", axis=1)
+    y_train = df["fare_amount"]
+    X_train = df.drop("fare_amount", axis=1)
 
     # hold out
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+    #X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
 
     # build pipeline
     trainer = Trainer(X_train, y_train)
+
+    score = trainer.cross_val_score()
+    print(f'RMSE of cross-validation: {round(score, 2)}')
 
     # train the pipeline
     trainer.run()
 
     # evaluate the pipeline
-    rmse = trainer.evaluate(X_val, y_val)
-    print(f'RMSE: {round(rmse, 2)}')
+    #rmse = trainer.predict(X_val)
+    #print(f'RMSE of X_val: {round(rmse, 2)}')
 
     trainer.save_model()
